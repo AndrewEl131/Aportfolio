deb03448fb6a740cdaaf6b8699d033b2d15a279d
AndrewEl131 <andyelizbarashvilii@gmail.com>
2025-12-18 16:42:13 +0400

    Deploy using docs folder

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
    base: '/Aportfolio/',
+   build: {
+    outDir: 'docs'
+  },
   plugins: [react(), tailwindcss()],
 })
